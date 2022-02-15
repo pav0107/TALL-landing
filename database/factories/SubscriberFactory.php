7eeaefcca998ce5cc61ef94ee21e4266b288f9fe
Pav Rao <pav0107@gmail.com>
2022-02-15 11:30:57 +0000

    additional setup

---
+++ database/factories/SubscriberFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Subscriber>
+ */
+class SubscriberFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => now(),
+        ];
+    }
+}
